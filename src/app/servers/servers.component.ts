bbc9443c8ccd8caa83079c00ce328ebff178c888
Yatin Dua <developerdua@gmail.com>
2024-04-23 21:13:57 +0530

    added component

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+//  selector: 'app-servers',
+  selector: '[app-servers]',
+  template: `
+    <p>hello servers works!</p>`,
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
